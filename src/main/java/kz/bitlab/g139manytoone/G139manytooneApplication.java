7661ef11474ca7046a0865584e839354b0e3b7d0
abylayquat22 <abylayquat22@gmail.com>
2025-10-11 17:29:07 +0500

    init

---
+++ src/main/java/kz/bitlab/g139manytoone/G139manytooneApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.g139manytoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G139manytooneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G139manytooneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bitlab/g139manytoone/G139manytooneApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.g139manytoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G139manytooneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
